2b95ca82d0ece51810e91658ac7b8f0104dcc4d8
moepyaePK <6531503158@lamduan.mfu.ac.th>
2023-11-15 15:48:48 +0700

    Co-authored-by: Siss Hmue Aung <siss-hmue@users.noreply.github.com>

--- src/main/java/th/mfu/game.java
+++ src/main/java/th/mfu/game.java
@@ -6,36 +6,6 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class game {
-    private String name;
-    Scanner scanner = new Scanner(System.in);
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static int generateId() {
-        Random random = new Random();
-        return random.nextInt(5);
-    }
-
-    public static int getGameId() {
-        return generateId();
-    }
-
-    public static int getPlayerXId() {
-        return generateId();
-    }
-
-    public static int getPlayerOId() {
-        return generateId();
-    }
-
-    public static void main(String args[]) {
-
-    }
+   
 
 }
