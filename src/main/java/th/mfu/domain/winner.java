5115654f48cab33751d2b9d94de9b87a4eee0f85
Meo-6531503185 <122962971+Meo-6531503185@users.noreply.github.com>
2023-11-25 21:36:27 +0700

    modified winner.java

--- src/main/java/th/mfu/domain/winner.java
+++ src/main/java/th/mfu/domain/winner.java
@@ -1,46 +1,30 @@
 package th.mfu.domain;
-
-
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-
 import javax.persistence.Id;
-
 import javax.persistence.OneToOne;
-
 @Entity
 public class winner {
      @Id
     private long id;  
     private String name;
-   
-
-
     @OneToOne(cascade =CascadeType.ALL )
     private player player;
-    
     public String getName() {
         return name;
     }
-
     public player getPlayer() {
         return player;
     }
-
     public void setPlayer(player player) {
         this.player = player;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
-    
     public long getId() {
         return id;
     }
-
-
     public void setId(long id) {
         this.id = id;
     }
