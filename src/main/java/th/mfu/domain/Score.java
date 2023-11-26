b91cb3a6ae4820797a80f21e04c980790f1b7f01
Eta6531503138 <6531503138@lamduan.mfu.ac.th>
2023-11-26 15:54:10 +0700

    done

--- src/main/java/th/mfu/domain/Score.java
+++ src/main/java/th/mfu/domain/Score.java
@@ -15,22 +15,4 @@ public class Score
     private game  game;
 
 }
-/*src/main/java/th/mfu/domain/Score.java
- * package th.mfu.domain;
 
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-
-@Entity
-public class Score
-{
-    @Id
-    private long id;
-
-    @ManyToOne(cascade = CascadeType.MERGE)
-    private game  game;
-
-}
- */
