f479cd7b7ccb9c9e213df2a62e72d8daa6643fb9
Meo-6531503185 <122962971+Meo-6531503185@users.noreply.github.com>
2023-11-26 15:26:15 +0700

    insert

--- src/main/java/th/mfu/domain/player.java
+++ src/main/java/th/mfu/domain/player.java
@@ -5,3 +5,46 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
+@Entity
+public class player 
+{
+     @Id
+    private long id;  
+    private String name;
+
+    @ManyToOne(cascade = CascadeType.MERGE)
+    private game  game;
+
+    public String getName() 
+    {
+        return name;
+    }
+    
+    public player(long id, String name) 
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public player()
+    {
+
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public long getId() 
+    {
+        return id;
+    }
+
+    public void setId(long id) 
+    {
+        this.id = id;
+    }
+
+    
+}
